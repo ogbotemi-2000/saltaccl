44c61a565f7aedaebaa2b1ce2e290bc7e9ae413f
ogbotemi-2000 <oogbotemi@gmail.com>
2024-02-21 19:00:10 -0800

    changes to footer in all pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+window.addEventListener('DOMContentLoaded', _=>animations.addRippleAnimations())
 function onScroll(page, mthd, sTop, t) {
   (t = onScroll).lastY||=0, sTop = page.scrollY,
   mthd = cLs, window.topCta&&['show', 'relative', 'absolute']
@@ -6,4 +7,42 @@ function onScroll(page, mthd, sTop, t) {
   }),
   setTimeout(_=>t.lastY=sTop);
   return t.lastY/innerHeight
+}
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of the white box if rendered for any reason.
+  ['color', 'background'].forEach(e=>textArea.style[e] = 'transparent'),
+  textArea.value = text;
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  let msg;
+  try {
+    var successful = document.execCommand('copy');
+    msg = successful ? 'copied' : 'failed to copy';
+  } catch (err) {
+    msg = 'cannot copy; no support'
+  }
+  document.body.removeChild(textArea);
+  return msg
 }
